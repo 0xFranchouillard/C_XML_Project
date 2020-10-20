37770edc646fb4f95c309779b0b51324c2a34cd5
CyrilleChampion <cyrillechampion13@gmail.com>
2020-10-20 19:41:24 +0200

    Hello, World !

---
+++ cmake-build-debug/CMakeFiles/C_XML_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_XML_Project.dir/main.c.obj"
+  "C_XML_Project.exe"
+  "C_XML_Project.exe.manifest"
+  "C_XML_Project.pdb"
+  "libC_XML_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_XML_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

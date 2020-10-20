6edb80c6410bf634fbc23e4fef1768784a97e38c
CyrilleChampion <cyrillechampion13@gmail.com>
2020-10-20 19:57:57 +0200

    read xml xml + dtd de test

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 int main() {
-    printf("Hello, World!\n");
+    char* line = malloc(sizeof(char) * 200);
+    FILE* fo = fopen("../file.xml","r+");
+
+    if(fo == NULL) return -1;
+
+    while((fgets(line,50,fo)) != NULL){
+        printf("%s",line);
+    }
+
+    fclose(fo);
+
+
+
+
+
     return 0;
 }
